3d8b3ee814732e9558eecfd492092eb6cbeee280
binmex <robin019@hotmail.com>
2022-03-19 17:56:37 -0500

    sss

--- js/loadData.php
+++ js/loadData.php
@@ -1,8 +1,6 @@
 <?php
 
-$file_json=__DIR__.'/files/Estudiantes.json';
-    $holis=file_get_contents($file_json);
-
-    var_dump($holis);
+//$file_json=__DIR__.'/files/Estudiantes.json';
+$holis=file_get_contents(__DIR__ . '/Estudiantes.json');
+var_dump($holis);
 $codigos=json_decode($holis,true);
-
